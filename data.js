1746c8e9dce540466c2f861a2be053fcf0e76ddc
MD RASHEDUZZAMAN <159575068+MrZaman03@users.noreply.github.com>
2026-05-03 21:19:06 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const siteData = {
     "profile": {
         "name": "MD RASHEDUZZAMAN",
-        "photo": "Weixin Image_20260502221445_127_2.lpg"
+        "photo": "Weixin Image_20260502221445_127_2.jpg"
         "major": "Artificial Intelligence",
         "year": "Undergraduate",
         "university": "Shanghai University of Engineering Science",
